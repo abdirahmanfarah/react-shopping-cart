776e9b78342b584e0bb1a3ab3a9faeeb8173a986
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-10-07 14:57:22 -0700

    added product and cartcontext

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//Contexts
+
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { tsPropertySignature } from '@babel/types';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
@@ -20,21 +26,28 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+		<ProductContext.Provider value={{ products, addItem }}>
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				/* Before */
+				// render={() => (
+				// 	<Products
+				// 		products={products}
+				// 		addItem={addItem}
+				// 	/>
+
+				/* After */
+				component={Products}
 			/>
+				<CartContext value ={cart}>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+						/>
+				</CartContext>
+		 </ProductContext.Provider>
 		</div>
 	);
 }
